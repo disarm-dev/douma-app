ee2ca5b80bb961dbeaea32d7c18147d1af807957
Nicolai Davies <nicolaidavies.nd@gmail.com>
2018-02-01 14:30:15 +0100

    Fix broken links

--- src/apps/foci/pages/front_page.vue
+++ src/apps/foci/pages/front_page.vue
@@ -4,10 +4,10 @@
 
     <ul>
       <li>
-        <router-link to="foci:list">List view</router-link>
+        <router-link :to="{name: 'foci:list'}">List view</router-link>
       </li>
       <li>
-        <router-link to="foci:map">Map view</router-link>
+        <router-link :to="{name: 'foci:map'}">Map view</router-link>
       </li>
       <li>
         <router-link :to="{ name: 'foci:detail', params: { foci_id: '123'}}">Test Detail view</router-link>
