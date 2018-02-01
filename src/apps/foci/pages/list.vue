9f8fca299e0e8051659a6e29a6f4c32a8adb7f4e
Nicolai Davies <nicolaidavies.nd@gmail.com>
2018-02-01 15:21:54 +0100

    Very simple testing in place

--- src/apps/foci/pages/list.vue
+++ src/apps/foci/pages/list.vue
@@ -2,7 +2,7 @@
   <div class="list">
     <h1>I am the list view. I render lists.</h1>
 
-    <v-client-table v-if="table_data.length" :data="table_data" :columns="table_columns"></v-client-table>
+    <v-client-table v-if="table_data.length" :data="table_data" :columns="table_columns" @row-click="handle_click"></v-client-table>
   </div>
 </template>
 
@@ -19,15 +19,29 @@
     computed: {
       table_data() {
         const case_clusters = this.$store.state.foci.case_clusters
-        return case_clusters.features.map(f => f.properties)
+        if (case_clusters && case_clusters.hasOwnProperty('features') && Array.isArray(case_clusters.features)) {
+          return case_clusters.features.map(f => f.properties)
+        } else {
+          return []
+        }
       },
       table_columns() {
         const case_clusters = this.$store.state.foci.case_clusters
-        const properties = case_clusters.features.map(f => f.properties)
-        return Object.keys(properties[0])
+
+        if (case_clusters && case_clusters.hasOwnProperty('features') && Array.isArray(case_clusters.features)) {
+          const properties = case_clusters.features.map(f => f.properties)
+          return Object.keys(properties[0])
+        } else {
+          return []
+        }
       }
     },
-    methods: {}
+    methods: {
+      handle_click({row, event}) {
+        // do something
+        console.log('row', row)
+      }
+    }
   }
 </script>
 
---
+++ test/specs/components/foci/list.test.js
@@ -0,0 +1,94 @@
+import test from 'ava'
+import {shallow, createLocalVue} from 'vue-test-utils'
+// import sinon from 'sinon'
+
+import foci_list from '../../../../src/apps/foci/pages/list.vue'
+
+const mock_store_empty = {
+  state: {
+    foci: {
+      case_clusters: {}
+    }
+  }
+}
+
+
+test('it renders', t => {
+  const wrapper = shallow(foci_list, {
+    mocks: {
+      $store: mock_store_empty
+    }
+  })
+  t.true(wrapper.exists())
+})
+
+
+test('table_columns should be an empty array when no case_clusters', t => {
+  const wrapper = shallow(foci_list, {
+    mocks: {
+      $store: mock_store_empty
+    }
+  })
+
+  t.deepEqual(wrapper.vm.table_columns, [])
+})
+
+test('table_columns should be an array of string when data is there', t => {
+  const wrapper = shallow(foci_list, {
+    mocks: {
+      $store: {
+        state: {
+          foci: {
+            case_clusters: {
+              features: [
+                {
+                  properties: {
+                    test_1: 1,
+                    test_2: 1
+                  }
+                }
+              ]
+            }
+          }
+        }
+      }
+    }
+  })
+
+  t.deepEqual(wrapper.vm.table_columns, ['test_1', 'test_2'])
+})
+
+
+test('table_data should be an empty array when no case_clusters', t => {
+  const wrapper = shallow(foci_list, {
+    mocks: {
+      $store: mock_store_empty
+    }
+  })
+
+  t.deepEqual(wrapper.vm.table_data, [])
+})
+
+test('table_data should be an array of objects when data is there', t => {
+  const wrapper = shallow(foci_list, {
+    mocks: {
+      $store: {
+        state: {
+          foci: {
+            case_clusters: {
+              features: [
+                {
+                  properties: {
+                    test_1: 1,
+                    test_2: 1
+                  }
+                }
+              ]
+            }
+          }
+        }
+      }
+    }
+  })
+  t.deepEqual(wrapper.vm.table_data, [{test_1: 1, test_2: 1}])
+})
