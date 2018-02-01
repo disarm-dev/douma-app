1292527e1c836e645c3569ee8a1fd7b2b2ddf3a3
Nicolai Davies <nicolaidavies.nd@gmail.com>
2018-02-01 14:30:23 +0100

    Add list

--- src/apps/foci/pages/list.vue
+++ src/apps/foci/pages/list.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    I am the list view. I render lists.
+  <div class="list">
+    <h1>I am the list view. I render lists.</h1>
+
+    <v-client-table v-if="table_data.length" :data="table_data" :columns="table_columns"></v-client-table>
   </div>
 </template>
 
@@ -14,10 +16,24 @@
     data() {
       return {}
     },
+    computed: {
+      table_data() {
+        const case_clusters = this.$store.state.foci.case_clusters
+        return case_clusters.features.map(f => f.properties)
+      },
+      table_columns() {
+        const case_clusters = this.$store.state.foci.case_clusters
+        const properties = case_clusters.features.map(f => f.properties)
+        return Object.keys(properties[0])
+      }
+    },
     methods: {}
   }
 </script>
 
 <style scoped>
-
+  .list {
+    max-width: 900px;
+    margin: 0 auto;
+  }
 </style>
