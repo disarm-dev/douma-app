7a07f2b65bff8b4fb64d0b40a8c8a5143eaec544
Nicolai Davies <nicolaidavies.nd@gmail.com>
2018-02-12 14:03:14 +0100

    validating clusters was throwing errors, not quite sure why, but this fixed

--- src/apps/foci/lib/validate_case_clusters.js
+++ src/apps/foci/lib/validate_case_clusters.js
@@ -3,19 +3,21 @@ import { case_cluster_schema } from './models/case_clusters/schema'
 import { case_schema } from './models/cases/schema'
 
 const ajv = new Ajv()
+const case_cluster_validate = ajv.compile(case_cluster_schema);
+const case_validate = ajv.compile(case_schema);
 
 export function validate_case_clusters(case_clusters) {
   for (const case_cluster of case_clusters) {
-    if (!ajv.validate(case_cluster_schema, case_cluster)) {
-      throw ajv.errors
+    if (!case_cluster_validate(case_cluster)) {
+      throw case_cluster_validate.errors
     }
   }
 }
 
 export function validate_cases(cases) {
   for (const single_case of cases) {
-    if (!ajv.validate(case_schema, single_case)) {
-      throw ajv.errors
+    if (!case_validate(single_case)) {
+      throw case_validate.errors
     }
   }
 }
