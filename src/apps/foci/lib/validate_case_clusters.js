ba712c19cc6ff7b6fb277bcffdab0a7e759da720
Nicolai Davies <nicolaidavies.nd@gmail.com>
2018-02-12 11:33:21 +0100

    validate_case according to schema

--- src/apps/foci/lib/validate_case_clusters.js
+++ src/apps/foci/lib/validate_case_clusters.js
@@ -1,12 +1,21 @@
 import Ajv from 'ajv'
 import { case_cluster_schema } from './models/case_clusters/schema'
+import { case_schema } from './models/cases/schema'
 
 const ajv = new Ajv()
 
 export function validate_case_clusters(case_clusters) {
-    for (const case_cluster of case_clusters) {
-        if (!ajv.validate(case_cluster_schema, case_cluster)) {
-            throw ajv.errors
-        }
+  for (const case_cluster of case_clusters) {
+    if (!ajv.validate(case_cluster_schema, case_cluster)) {
+      throw ajv.errors
     }
+  }
+}
+
+export function validate_cases(cases) {
+  for (const single_case of cases) {
+    if (!ajv.validate(case_schema, single_case)) {
+      throw ajv.errors
+    }
+  }
 }
