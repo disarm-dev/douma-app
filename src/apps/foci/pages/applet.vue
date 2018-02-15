8242dc37d59d5e2f26dbceccb615f47d40b1df52
Nicolai Davies <nicolaidavies.nd@gmail.com>
2018-02-15 11:49:03 +0100

    Insert fake filters that aren't working. Closes #9

--- src/apps/foci/pages/applet.vue
+++ src/apps/foci/pages/applet.vue
@@ -1,10 +1,41 @@
 <template>
   <div>
-    <div>
-      <md-button @click="get_case_clusters">
-        Get remote case clusters 
-      </md-button>
-    </div>
+    <md-toolbar>
+      <div class="md-toolbar-container">
+        <div class="filter_name">
+          <span>Status:</span>
+        </div>
+
+        <div>
+          <md-input-container>
+            <md-select>
+              <md-option value="inactive">inactive</md-option>
+              <md-option value="active">active</md-option>
+              <md-option value="cleared">cleared</md-option>
+            </md-select>
+          </md-input-container>
+        </div>
+
+        <div class="filter_name">
+          <span>Investigation Status:</span>
+        </div>
+
+        <div>
+          <md-input-container>
+            <md-select>
+              <md-option value="investigated">investigated</md-option>
+              <md-option value="suggested">suggested</md-option>
+              <md-option value="visual_review">visual review</md-option>
+            </md-select>
+          </md-input-container>
+        </div>
+      </div>
+
+    </md-toolbar>
+
+    <md-button @click="get_case_clusters">
+      Get remote case clusters 
+    </md-button>
 
     <router-view></router-view>
   </div>
@@ -13,15 +44,20 @@
 <script>
 export default {
   created() {
-    this.get_local()
+    this.get_local();
   },
   methods: {
     get_case_clusters() {
-      this.$store.dispatch('foci/get_case_clusters')
+      this.$store.dispatch("foci/get_case_clusters");
     },
     get_local() {
-      this.$store.dispatch('foci/get_case_clusters_local')
+      this.$store.dispatch("foci/get_case_clusters_local");
     }
   }
-}
+};
 </script>
+<style>
+  .filter_name {
+    margin: 0 10px;
+  }
+</style>
