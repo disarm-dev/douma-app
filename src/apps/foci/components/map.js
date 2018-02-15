f4a546711a04f41f359eead2d04d91bb2cc3ba50
Nicolai Davies <nicolaidavies.nd@gmail.com>
2018-02-15 13:37:12 +0100

    very simple, but composable map

---
+++ src/apps/foci/components/map.js
@@ -0,0 +1,57 @@
+import CONFIG from 'config/common'
+import mapboxgl from 'mapbox-gl'
+import uuid from 'uuid/v4'
+
+// This is mostly copied from 'lib/helpers/basic_map'
+// It's been duplicated in order to experiment with a new approach to composing maps
+
+export function render_map(map_container_id) {
+  return new Promise((resolve) => {
+
+    const map = new mapboxgl.Map({
+      container: map_container_id,
+      style: CONFIG.basemap.default.style,
+      center: CONFIG.basemap.default.coords,
+      zoom: CONFIG.basemap.default.zoom
+    })
+    
+    // disable zooming with mouse scroll. now you can scroll
+    map.scrollZoom.disable()
+    
+    map.on('error', (e) => {
+      console.warn('🗺 Basic map error:', e)
+      if (e.error) console.log(e.error)
+      // TODO: handle errors better
+    })
+    
+    map.addControl(new mapboxgl.NavigationControl())
+    
+    map.on('load', () => {
+      resolve(map)  
+    }) 
+  })
+}
+
+export function add_polygon_layer(map, feature_collection) {
+  // Could check if feature collection has points, or polygons
+  // Then we only need one function
+  const id = uuid()
+
+  map.addLayer({
+    id: id,
+    type: 'fill',
+    source: {
+      type: 'geojson',
+      data: feature_collection
+    },
+    paint: {
+      'fill-opacity': 0.2,
+    //   'fill-color': {
+    //     property: layer_string,
+    //     stops: palette
+    //   },
+    //   'fill-outline-color': 'black'
+    }
+  })
+  return id
+}
--- src/apps/foci/pages/map.vue
+++ src/apps/foci/pages/map.vue
@@ -1,23 +1,27 @@
 <template>
-  <div>
-    I am the map view. Guess what I render?
-  </div>
+  <div id="map"></div>
 </template>
 
 <script>
-  //import { mapState, mapActions, mapMutations } from 'vuex'
+  import {render_map, add_polygon_layer} from '../components/map'
 
+  let map 
   export default {
     name: 'map',
-    mounted() {
-    },
     data() {
       return {}
+    },
+    async mounted() {
+      const map = await render_map('map')
+      const case_clusters_feature_collection = await this.$store.dispatch('foci/get_case_clusters_fc')
+      const layer_id = add_polygon_layer(map, case_clusters_feature_collection)
     },
     methods: {}
   }
 </script>
 
 <style scoped>
-
+  #map {
+    height: 800px;
+  }
 </style>
