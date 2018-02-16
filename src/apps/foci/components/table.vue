bd3f9dddba805dab868ddee8d0e079e3f8fb8660
Nicolai Davies <nicolaidavies.nd@gmail.com>
2018-02-16 13:03:58 +0100

    Make list pretty

---
+++ src/apps/foci/components/table.vue
@@ -0,0 +1,26 @@
+<template>
+  <md-table>
+    <md-table-header>
+      <md-table-row>
+        <md-table-head v-for="column of table_columns" :key="column">{{column}}</md-table-head>
+      </md-table-row>
+    </md-table-header>
+
+    <md-table-body>
+      <md-table-row v-for="row in table_data" :key="row._id" @click="click(row)">
+        <md-table-cell v-for="column of table_columns" :key="column" md-numeric>{{row[column]}}</md-table-cell>
+      </md-table-row>
+    </md-table-body>
+  </md-table>
+</template>
+<script>
+export default {
+  props: ['table_columns', 'table_data'],
+  methods: {
+    click(row) {
+      this.$emit('click', row)
+    }
+  }
+}
+</script>
+
--- src/apps/foci/pages/list.vue
+++ src/apps/foci/pages/list.vue
@@ -1,24 +1,35 @@
 <template>
   <div class="list">
-    <h1>I am the list view. I render lists.</h1>
 
-    <v-client-table v-if="table_data.length" :data="table_data" :columns="table_columns" @row-click="handle_click"></v-client-table>
-
-    <div>
-      <pivot-table :data="table_data">
-
-      </pivot-table>
+    <div class="table-container">
+      <md-table-card>
+        <md-toolbar>
+          <h1 class="md-title">Case clusters</h1>
+        </md-toolbar>
+        <pretty-table v-if="table_data.length" :table_data="table_data" :table_columns="table_columns" @click="handle_click"></pretty-table>
+      </md-table-card>
     </div>
+
+    <div class="table-container">
+      <md-table-card>
+        <md-toolbar>
+          <h1 class="md-title">Summary</h1>
+        </md-toolbar>
+        <pivot-table :data="table_data"></pivot-table>
+      </md-table-card>
+    </div>
+
   </div>
 </template>
 
 <script>
   //import { mapState, mapActions, mapMutations } from 'vuex'
   import pivot_table from '../components/pivot_table'
+  import pretty_table from '../components/table'
 
   export default {
     name: 'list',
-    components: {'pivot-table': pivot_table},
+    components: {'pivot-table': pivot_table, 'pretty-table': pretty_table},
     computed: {
       table_data() {
         const case_clusters = this.$store.state.foci.case_clusters
@@ -29,10 +40,12 @@
         }
       },
       table_columns() {
+        const ignored_columns = ['geometry']
         const case_clusters = this.$store.state.foci.case_clusters
 
         if (case_clusters && case_clusters.length) {
-          return Object.keys(case_clusters[0])
+          const columns = Object.keys(case_clusters[0])
+          return columns.filter(column => !ignored_columns.includes(column))
         } else {
           return []
         }
@@ -50,5 +63,9 @@
   .list {
     max-width: 900px;
     margin: 0 auto;
+  }
+  
+  .table-container {
+    margin: 3em 0;
   }
 </style>
