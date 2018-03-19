fcb2d73a2edf55460cd8145e89b5789ce67137fb
Nicolai Davies <nicolaidavies.nd@gmail.com>
2018-03-19 10:43:14 +0100

    always tries to retrieve latest config

--- src/lib/models/instance_config/model.js
+++ src/lib/models/instance_config/model.js
@@ -4,13 +4,22 @@ import { read_instance_config } from './remote'
 const local_config_db = new InstanceConfigLocal()
 
 export async function read_instance_congfiguration_for(config_id) {
-  const local_instance_config = await local_config_db.read(config_id)
-
-  if (local_instance_config) {
-    return local_instance_config
-  } else {
-    const remote_instance_config = await read_instance_config(config_id)
-    await local_config_db.update(remote_instance_config)
+  try {
+    const remote_instance_config = await retrieve_and_store_remote_config(config_id)
     return remote_instance_config
+  } catch (e) {
+    const local_instance_config = await retrieve_local_config(config_id)
+    return local_instance_config
   }
+}
+
+async function retrieve_local_config(config_id) {
+  const local_instance_config = await local_config_db.read(config_id)
+  return local_instance_config
+}
+
+async function retrieve_and_store_remote_config(config_id) {
+  const remote_instance_config = await read_instance_config(config_id)
+  await local_config_db.update(remote_instance_config)
+  return remote_instance_config
 }
