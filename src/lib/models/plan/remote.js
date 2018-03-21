6241d072de7db43bdf8dc86a502c9a28a23ba60e
Sihle Mbhamali <mbhamalimosaic@gmail.com>
2018-03-21 15:43:47 +0200

    Changes include fetching specific plan

--- src/lib/models/plan/remote.js
+++ src/lib/models/plan/remote.js
@@ -1,6 +1,6 @@
 import {request_handler} from 'lib/remote/request-handler'
 
-export default {read_plan_current, create_plan}
+export default {read_plan_current, create_plan, read_plan_list, read_plan_detail}
 
 // PLANS
 function read_plan_current() {
@@ -8,9 +8,33 @@ function read_plan_current() {
   return request_handler(request)
 }
 
+function read_plan_detail(plan_id) {
+  const request = _read_plan_detail(plan_id)
+  return request_handler(request)
+}
+
+function read_plan_list() {
+  const request = _read_plan_list()
+  return request_handler(request)
+}
+
 function _read_plan_current() {
   return {
     url_suffix: '/plan/current',
+    timeout: 10000
+  }
+}
+
+function _read_plan_detail(plan_id) {
+  return {
+    url_suffix: `/plan/detail/${plan_id}`,
+    timeout: 10000
+  }
+}
+
+function _read_plan_list() {
+  return {
+    url_suffix: '/plan/list',
     timeout: 10000
   }
 }
