425e00d1e3fc13bb3b67a3c15b8f3266fa65e7d4
Nicolai Davies <nicolaidavies.nd@gmail.com>
2018-03-23 09:08:32 +0100

    Pass plans as prop

--- src/apps/irs_monitor/pages/controls/plans.vue
+++ src/apps/irs_monitor/pages/controls/plans.vue
@@ -3,7 +3,7 @@
     <h4>Plan</h4>
     <md-input-container>
       <label>Select plan</label>
-      <md-select v-model="plan">
+      <md-select>
         <md-option v-for="plan in plans" :key="plan._id" :value="plan._id" @selected="get_plan(plan)">
           {{plan.date | format_date }}, {{plan.targets}} areas
         </md-option>
@@ -19,21 +19,8 @@
   import moment from 'moment'
 
   export default {
-    // TODO: Pass in data as a prop
-    computed: {
-      ...mapState({
-        plans: state => state.irs_monitor.plans,
-      })/*,
-      plan: {
-        get() {
-          return (this.$store.state.irs_monitor.plan || {} )._id
-        },
-        set(plan_id) {
-          // TODO: Do this in a commit, not the component
-          const plan = this.$store.state.irs_monitor.plans.find(plan => plan._id === plan_id)
-          this.$store.commit('irs_monitor/set_plan', plan)
-        }
-      }*/
+    props: {
+      'plans': Array
     },
     filters: {
       format_date(date_int) {
