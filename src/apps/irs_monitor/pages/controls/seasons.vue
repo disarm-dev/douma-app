1f83716d297aecebf27e92f21abc92dd7eacdc99
Nicolai Davies <nicolaidavies.nd@gmail.com>
2018-04-05 14:23:31 +0200

    set default season

--- src/apps/irs_monitor/pages/controls/seasons.vue
+++ src/apps/irs_monitor/pages/controls/seasons.vue
@@ -3,12 +3,15 @@
     <h4>Seasons</h4>
     <md-input-container>
       <label>Select season start date</label>
-      <md-select>
+      <md-select :value="selected_start_date">
         <md-option v-for="(start_date, index) in season_start_dates" :key="start_date" :value="start_date" @selected="add_temporal_filter(start_date, index)">
           {{start_date | format_date }}
         </md-option>
       </md-select>
     </md-input-container>
+    <md-button @click="set_default_season">
+      Set default season
+    </md-button>
   </div>
 </template>
 <script>
@@ -17,6 +20,11 @@
     props: {
       season_start_dates: Array
     },
+    data() {
+      return {
+        selected_start_date: ''
+      }
+    },
     filters: {
       format_date(date_int) {
         return moment(date_int).format('DD MMMM YYYY')
@@ -24,6 +32,7 @@
     },
     methods: {
       add_temporal_filter(start_date, index) {
+        this.selected_start_date = start_date
         let end_date
 
         // is last date
@@ -48,6 +57,11 @@
           value: new Date(end_date).getTime(),
           display_value: moment(new Date(end_date)).format("MMM Do YYYY")
         })
+      },
+      set_default_season() {
+        const index = this.season_start_dates.length -  1
+        const start_date = this.season_start_dates[index]
+        this.add_temporal_filter(start_date, index)
       }
     }
   }
