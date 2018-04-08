16037001f576781ac2c62d07498b76a511885e9b
Jonathan Smith <jonathan@peoplesized.com>
2018-04-08 10:55:25 +0200

    stub test file

---
+++ test/specs/lib_helpers/csv_export.test.js
@@ -0,0 +1,11 @@
+import test from 'ava'
+
+// check input
+// process
+// format result
+
+test('empty object returns empty array', t => {
+  const expected = true
+  const value = true
+  t.is(value, expected, 'message');
+})
