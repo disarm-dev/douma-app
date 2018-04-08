c0e699c96373efa08e006a0c3038511ca02cf6a8
Jonathan Smith <jonathan@peoplesized.com>
2018-04-08 11:31:02 +0200

    steps outline

--- test/specs/lib_helpers/csv_export.test.js
+++ test/specs/lib_helpers/csv_export.test.js
@@ -1,5 +1,11 @@
 import test from 'ava'
 
+// steps
+// 0. take array of objects of different shapes
+// 1. create array of unique fields from all records
+// 2. iterate all records to create a row for each, using fields
+// 3. return array of rows with header row (using lib?)
+
 // check input
 // process
 // format result
