766652dd7c0c174fc2d20d9a6f3453d4d075eed0
Jonathan Smith <jonathan@peoplesized.com>
2018-04-10 10:13:49 +0200

    get json2csv to do the work

--- src/lib/helpers/csv-export/process.js
+++ src/lib/helpers/csv-export/process.js
@@ -1,36 +1,9 @@
-import flatten from 'flat'
-import jsoncsv from 'json-csv'
+const json2csv = require('json2csv').parse;
 
 export function process(json_array) {
   // flatten every record
-  const flattened = json_array.map(record => flatten(record))
-  const fields = unique_fields(flattened)
-  const options = create_options(fields)
+  const opts = {flatten: true}
 
   // convert to CSV string from JSON using options
-  jsoncsv.csvBuffered(json_array, options)
-}
-
-/**
- * Convert array of field names to the `options` for `json-csv`
- * @param fields_array
- * @returns {{fields: *}}
- */
-export function create_options(fields_array) {
-  // Drop any blanks, create array
-  const fields = fields_array.filter(i => i).map(field => {
-    return {name: field}
-  })
-
-  return {
-    fields: fields
-  }
-}
-
-export function unique_fields(json_array) {
-  let fields = new Set
-  json_array.forEach(record => {
-    Object.keys(record).forEach(key => fields.add(key))
-  })
-  return Array.from(fields)
+  return json2csv(json_array, opts)
 }
--- test/specs/lib_helpers/csv_export/process.test.js
+++ test/specs/lib_helpers/csv_export/process.test.js
@@ -4,5 +4,16 @@ import {process} from 'lib/helpers/csv-export/process'
 
 test('simple array', t => {
   const json_array = [{a: 1}, {a: 2, b: 3}]
-  t.pass()
+  const actual = process(json_array)
+  const expected = '"a","b"\n1,\n2,3'
+
+  t.is(actual, expected)
+})
+
+test('nested', t => {
+  const json_array = [{a: 1, nested: {b: 2}},{a: 2}]
+  const actual = process(json_array)
+  const expected = '"a","nested.b"\n1,2\n2,'
+
+  t.is(actual, expected)
 })
