8e67711e3580f29e97c5fa84b53daf8a0e473d8d
Jonathan Smith <jonathan@peoplesized.com>
2018-04-13 08:38:02 +0200

    remove 'guess'

--- src/apps/irs_record_point/store.js
+++ src/apps/irs_record_point/store.js
@@ -10,16 +10,10 @@ import moment from 'moment'
 
 const controller = new ResponseController('record')
 
-
 export default {
   namespaced: true,
-  unpersisted_state_keys: ['responses'],
   state: {
-    responses: [],
-    guessed_responses:0,
-    responses_not_in_village:0,
-
-    // Not pure metadata, but we want to persist between each form entry
+    // Kind-of metadata we want to persist between each form entry
     persisted_metadata: {},
   },
   mutations: {
@@ -34,42 +28,6 @@ export default {
     set_team_name: (state, team_name) => {
       state.persisted_metadata.team_name = team_name
     },
-
-    // Guessing
-    clear_guessed_responses: (state) => {
-      state.guessed_responses = 0
-    },
-    clear_responses_not_inVillage: (state) => {
-      state.responses_not_in_village = 0
-    },
-    add_guessed_responses: (state, responses) => {
-      state.guessed_responses += responses
-    },
-    add_fixes: (state, fixes) => {
-      state.responses_not_in_village += fixes
-    },
-
-    update_responses: (state, responses) => {
-      for (const response of responses) {
-        let index = state.responses.findIndex((r) => r.id === response.id)
-        state.responses.splice(index, 1, response)
-      }
-    },
-
   },
-  actions: {
-    guess_response_locations: async (context,responses) => {
-      let guessed_location_responses = responses
-      try {
-        guessed_location_responses = guess_location_for(guessed_location_responses)
-        await controller.create_or_update_bulk_local(guessed_location_responses)
-        context.commit('update_responses', guessed_location_responses)
-        context.commit('root:set_snackbar', {message: 'Finished guessing locations'}, {root: true})
-      } catch (e) {
-        Raven.captureException(e)
-        console.error(e)
-      }
-    },
-  }
 }
 
