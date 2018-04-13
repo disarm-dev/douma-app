876a20fdb5daaf8781ffc0ca3ac05d9bc2492a9e
Jonathan Smith <jonathan@peoplesized.com>
2018-04-13 20:33:38 +0200

    refac to reduce

--- src/lib/instance_data/form_helpers.js
+++ src/lib/instance_data/form_helpers.js
@@ -1,18 +1,13 @@
 const get_form_elements = (form) => {
-  let arr = []
-  form.pages.forEach((page, i) => {
-    if (page.elements)
-    page.elements.forEach(element => {
-      if (arr.find(i => i.name === element.name)) return
-
-      arr.push({
-        page: i,
-        name: element.name,
-        type: element.type
-      })
-    })
-  })
-  return arr
+  return form.pages.reduce((acc, page, page_number) => {
+    return acc.concat(page.elements.map(elm => {
+      return {
+        page: page_number,
+        name: elm.name,
+        type: elm.type
+      }
+    }))
+  }, [])
 }
 
 const get_form_fields = (form) => {
