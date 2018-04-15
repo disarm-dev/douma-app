c1e5d3c469bd1dafb07b33f48c10d68cb294f8fd
Jonathan Smith <jonathan@peoplesized.com>
2018-04-15 07:20:10 +0200

    holy cow, geodata is 'got' a lot for even irs_record

--- src/config/cache.js
+++ src/config/cache.js
@@ -1,8 +1,17 @@
 const cache = {
-  geodata: {
-    // e.g. clusters: null // FeatureCollection
-  },
 }
+
+// e.g. clusters: null // FeatureCollection
+Object.defineProperty(cache, 'geodata', {
+  get: function () {
+    console.trace('get geodata')
+    return this._geodata
+  },
+  set: function (val) {
+    this._geodata = val
+  }
+})
+
 // TODO: @debug Remove this global when we no longer need our training wheels
 window.__disarm_debug_cache = cache
 
