5dc7bee0d86992516df8fc41f428a053bd243b5a
Jonathan Smith <jonathan@peoplesized.com>
2018-04-15 20:25:04 +0200

    broke cache with incorrect proxy test

--- src/config/cache.js
+++ src/config/cache.js
@@ -1,16 +1,8 @@
 const cache = {
-}
-
-// e.g. clusters: null // FeatureCollection
-Object.defineProperty(cache, 'geodata', {
-  get: function () {
-    console.trace('get geodata')
-    return this._geodata
-  },
-  set: function (val) {
-    this._geodata = val
+  // e.g. clusters: null // FeatureCollection
+  geodata: {
   }
-})
+}
 
 // TODO: @debug Remove this global when we no longer need our training wheels
 window.__disarm_debug_cache = cache
