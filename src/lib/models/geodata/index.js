d9f2ce00520c313e0b34134dfab22fa74622e6bd
Jonathan Smith <jonathan@peoplesized.com>
2018-04-15 21:55:28 +0200

    start index.js for geodata functions

---
+++ src/lib/models/geodata/index.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * @returns geodata object
+ */
+function get_geodata() {
+  return geodata
+}
+
+/**
+ *
+ * @param geodata
+ * @returns {boolean}
+ */
+function set_geodata(geodata) {
+  geodata.length
+  return false
+}
+
+/**
+ *
+ * @returns {boolean}
+ */
+function geodata_valid() {
+  return false
+}
+
+export {geodata_valid}
