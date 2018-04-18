8e68a7ce0dee9a513587963f67315bd54aa7b565
Jonathan Smith <jonathan@peoplesized.com>
2018-04-18 11:39:49 +0200

    async await version of need_to_update

--- src/lib/remote/check-application-version.js
+++ src/lib/remote/check-application-version.js
@@ -5,19 +5,20 @@ import BUILD_TIME from 'config/build-time'
  * Returns object containing {status: ['CAN_UPDATE', 'ON_LATEST' or 'NO_RESPONSE'], local_version: version, remote_version: version}
  * @returns
  */
-const need_to_update = () => {
-  return get_version().then((remote_version) => {
-    if (remote_version && (remote_version !== BUILD_TIME.VERSION_COMMIT_HASH_SHORT)) {
+const need_to_update = async () => {
+  try {
+    const remote_version = get_version()
+    if (rcemote_version && (remote_version !== BUILD_TIME.VERSION_COMMIT_HASH_SHORT)) {
       console.log(`🔺 DiSARM version check: New version available ${remote_version}, can/should update.`)
       return {status: 'CAN_UPDATE', local_version: BUILD_TIME.VERSION_COMMIT_HASH_SHORT, remote_version: remote_version}
     } else {
-      console.log("✅ DiSARM version check: Already running most recent version")
+      console.log('✅ DiSARM version check: Already running most recent version')
       return {status: 'ON_LATEST', local_version: BUILD_TIME.VERSION_COMMIT_HASH_SHORT, remote_version: remote_version}
     }
-  }).catch(err => {
-    console.log("🤷‍ DiSARM version check: No information on new version (network request failed)")
-    return {status: "NO_RESPONSE", local_version: BUILD_TIME.VERSION_COMMIT_HASH_SHORT}
-  })
+  } catch (err) {
+    console.log('🤷‍ DiSARM version check: No information on new version (network request failed)')
+    return {status: 'NO_RESPONSE', local_version: BUILD_TIME.VERSION_COMMIT_HASH_SHORT}
+  }
 }
 
 export {need_to_update}
