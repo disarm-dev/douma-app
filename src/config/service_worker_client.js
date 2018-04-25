55fa08eebad9c990ec3e7ce4ddc7c4acaa7c811a
Jonathan Smith <jonathan@peoplesized.com>
2018-04-25 13:12:29 +0200

    let's check we're in production mode (not dev), and not try to load SW from dev server

--- src/config/service_worker_client.js
+++ src/config/service_worker_client.js
@@ -1,25 +1,26 @@
 import BUILD_TIME from 'config/build-time'
 import {register} from 'register-service-worker'
 
-if (!BUILD_TIME.DOUMA_PRODUCTION_MODE) {
+if (BUILD_TIME.DOUMA_PRODUCTION_MODE) {
+  register('/service-worker.js', {
+    ready() {
+      console.log('[sw] Service worker is active.');
+    },
+    cached() {
+      console.log('[sw] Content has been cached for offline use.');
+    },
+    updated() {
+      document.dispatchEvent(new Event('show-update-available'))
+      console.log('[sw] New content is available; please refresh.');
+    },
+    offline() {
+      console.log('[sw] No internet connection found. App is running in offline mode.');
+    },
+    error(error) {
+      console.error('[sw] Error during service worker registration:', error);
+    }
+  });
+} else {
   console.warn('DOUMA ServiceWorker disabled in development')
 }
 
-register("/service-worker.js", {
-  ready() {
-    console.log("[sw] Service worker is active.");
-  },
-  cached() {
-    console.log("[sw] Content has been cached for offline use.");
-  },
-  updated() {
-    document.dispatchEvent(new Event('show-update-available'))
-    console.log("[sw] New content is available; please refresh.");
-  },
-  offline() {
-    console.log("[sw] No internet connection found. App is running in offline mode.");
-  },
-  error(error) {
-    console.error("[sw] Error during service worker registration:", error);
-  }
-});
