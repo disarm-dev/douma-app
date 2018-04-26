37fc7fc1ae9bfbc1cf1c52982734c30468b7ceaf
Sihle Mbhamali <mbhamalimosaic@gmail.com>
2018-04-26 11:23:29 +0200

    Corrected seasons filters

--- src/apps/irs_monitor/pages/controls/seasons.vue
+++ src/apps/irs_monitor/pages/controls/seasons.vue
@@ -4,7 +4,7 @@
     <md-input-container>
       <label>Select season start date</label>
       <md-select :value="selected_start_date">
-        <md-option v-for="(start_date, index) in season_start_dates" :key="start_date" :value="start_date" @selected="add_temporal_filter(start_date, index)">
+        <md-option v-for="(start_date, index) in season_start_dates" :key="start_date" :value="start_date" @selected="set_season(start_date, index)">
           {{start_date | format_date }}
         </md-option>
       </md-select>
@@ -16,6 +16,8 @@
 </template>
 <script>
   import moment from 'moment'
+  import {isEqual} from 'lodash'
+
   export default {
     props: {
       season_start_dates: Array
@@ -31,37 +33,60 @@
       }
     },
     methods: {
-      add_temporal_filter(start_date, index) {
-        this.selected_start_date = start_date
-        let end_date
+      set_season(start_date, index) {
+        /*
+        The season is inclusive of the season start date
+        The season is exclusive of the season end date, unless it is the last season
+         */
+        const start_comparator = '>='
+        const end_comparator = '<='
 
-        // is last date
-        if (index === 0) {
-          end_date = new Date()
-        } else { // is not last date
-          end_date = this.season_start_dates[index-1]
+        const filters = this.$store.state.irs_monitor.filters
+
+        const existing_start_date_filter = filters.find(f => isEqual(f.name, 'recorded_on') && isEqual(f.comparator, start_comparator))
+        const existing_end_date_filter = filters.find(f => isEqual(f.name, 'recorded_on') && isEqual(f.comparator, end_comparator))
+
+        if (existing_start_date_filter) {
+          // remove filter
+          this.$store.commit('irs_monitor/remove_filter', existing_start_date_filter)
         }
+
+        if (existing_end_date_filter) {
+          // remove filter
+          this.$store.commit('irs_monitor/remove_filter', existing_end_date_filter)
+        }
+
+        // set UI date
+        this.selected_start_date = start_date
 
         // emit start
         this.$store.commit('irs_monitor/add_filter', {
           name: 'recorded_on',
-          comparator: '>',
+          comparator: start_comparator,
           value: new Date(start_date).getTime(),
           display_value: moment(new Date(start_date)).format("MMM Do YYYY")
         })
 
-        //emit end
-        this.$store.commit('irs_monitor/add_filter', {
-          name: 'recorded_on',
-          comparator: '<',
-          value: new Date(end_date).getTime(),
-          display_value: moment(new Date(end_date)).format("MMM Do YYYY")
-        })
+        // is last date
+        if (index > 0) { // is not first date
+          const end_date = moment(this.season_start_dates[index - 1]).subtract(1, 'day')
+          //emit end
+          this.$store.commit('irs_monitor/add_filter', {
+            name: 'recorded_on',
+            comparator: end_comparator,
+            value: new Date(end_date).getTime(),
+            display_value: end_date.format("MMM Do YYYY")
+          })
+        }
+
+
+
+
       },
       set_default_season() {
         const index = this.season_start_dates.length -  1
         const start_date = this.season_start_dates[0]
-        this.add_temporal_filter(start_date, 0)
+        this.set_season(start_date, 0)
       }
     }
   }
