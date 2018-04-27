31b92aa198af8cdc6b24733e4e04ae0baf2a250f
Sihle Mbhamali <mbhamalimosaic@gmail.com>
2018-04-27 09:15:58 +0200

    Fixing failing test

--- test/specs/components/irs_monitor/seasons.test.js
+++ test/specs/components/irs_monitor/seasons.test.js
@@ -28,7 +28,7 @@ test('clicking an option calls add_temporal_filter', async t => {
       season_start_dates: ['2018-01-01', '2018-02-01']
     },
     methods: {
-      add_temporal_filter: spy
+      set_season: spy
     }
   })
 
@@ -40,10 +40,15 @@ test('clicking an option calls add_temporal_filter', async t => {
   t.is(spy.getCall(0).args[1], 0)
 })
 
-test('calling add_temporal_filter with the last date sets start_date to start_date and end_date to now', async t => {
+test('calling set_season with the last date sets start_date to start_date and end_date to now', async t => {
   const spy = sinon.spy()
   const mock_store = {
-    commit: spy
+    commit: spy,
+    state: {
+      irs_monitor:{
+        filters:[]
+      }
+    }
   }
 
   const wrapper = shallow(Seasons, {
@@ -55,25 +60,26 @@ test('calling add_temporal_filter with the last date sets start_date to start_da
     }
   })
   const date_to_test_against = +new Date()
-  wrapper.vm.add_temporal_filter('2018-02-01', 1)
+  wrapper.vm.set_season('2018-02-01', 1)
 
   t.true(spy.calledTwice)
   t.is(spy.getCall(0).args[0], 'irs_monitor/add_filter')
   t.is(spy.getCall(0).args[1].name, 'recorded_on')
-  t.is(spy.getCall(0).args[1].comparator, '>')
+  t.is(spy.getCall(0).args[1].comparator, '>=')
   t.is(spy.getCall(0).args[1].value, new Date('2018-02-01').getTime())
 
-  
+
   t.is(spy.getCall(1).args[0], 'irs_monitor/add_filter')
   t.is(spy.getCall(1).args[1].name, 'recorded_on')
-  t.is(spy.getCall(1).args[1].comparator, '<')
-  
+  t.is(spy.getCall(1).args[1].comparator, '<=')
+
   // This is really hard to test, if we just used equal it would fail on a slow machine
-  t.true(spy.getCall(1).args[1].value >= date_to_test_against)
+  console.log(spy.getCall(1).args[1].value,date_to_test_against)
+  t.true(spy.getCall(1).args[1].value <= date_to_test_against)
 })
 
 
-test('calling add_temporal_filter with the first date sets start_date to start_date and end_date to the next start_date', async t => {
+test('calling set_season with the first date sets start_date to start_date and end_date to the next start_date', async t => {
   const spy = sinon.spy()
   const mock_store = {
     commit: spy
@@ -87,7 +93,7 @@ test('calling add_temporal_filter with the first date sets start_date to start_d
       $store: mock_store
     }
   })
-  wrapper.vm.add_temporal_filter('2018-01-01', 0)
+  wrapper.vm.set_season('2018-01-01', 0)
 
 
   t.true(spy.calledTwice)
