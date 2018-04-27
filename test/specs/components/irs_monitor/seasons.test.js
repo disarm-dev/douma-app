d687ea69f0e4d39f434ae6f3031cf597bfbdd0bf
Sihle Mbhamali <mbhamalimosaic@gmail.com>
2018-04-27 10:33:09 +0200

    Testing for end_date <= next_seasons_start_date_minus_a_day

--- test/specs/components/irs_monitor/seasons.test.js
+++ test/specs/components/irs_monitor/seasons.test.js
@@ -1,6 +1,7 @@
 import test from 'ava'
 import { shallow, mount } from 'vue-test-utils'
 import sinon from 'sinon'
+import moment from 'moment'
 import Seasons from 'apps/irs_monitor/pages/controls/seasons.vue'
 
 test('it renders', async t => {
@@ -74,7 +75,6 @@ test('calling set_season with the last date sets start_date to start_date and en
   t.is(spy.getCall(1).args[1].comparator, '<=')
 
   // This is really hard to test, if we just used equal it would fail on a slow machine
-  console.log(spy.getCall(1).args[1].value,date_to_test_against)
   t.true(spy.getCall(1).args[1].value <= date_to_test_against)
 })
 
@@ -82,31 +82,36 @@ test('calling set_season with the last date sets start_date to start_date and en
 test('calling set_season with the first date sets start_date to start_date and end_date to the next start_date', async t => {
   const spy = sinon.spy()
   const mock_store = {
-    commit: spy
+    commit: spy,
+    state: {
+      irs_monitor:{
+        filters:[]
+      }
+    }
   }
 
   const wrapper = shallow(Seasons, {
     propsData: {
-      season_start_dates: ['2018-01-01', '2018-02-01']
+      season_start_dates: [ '2018-02-01','2018-01-01']
     },
     mocks: {
       $store: mock_store
     }
   })
-  wrapper.vm.set_season('2018-01-01', 0)
+  wrapper.vm.set_season('2018-01-01', 1)
 
 
   t.true(spy.calledTwice)
   t.is(spy.getCall(0).args[0], 'irs_monitor/add_filter')
   t.is(spy.getCall(0).args[1].name, 'recorded_on')
-  t.is(spy.getCall(0).args[1].comparator, '>')
+  t.is(spy.getCall(0).args[1].comparator, '>=')
   t.is(spy.getCall(0).args[1].value, new Date('2018-01-01').getTime())
 
 
 
   t.is(spy.getCall(1).args[0], 'irs_monitor/add_filter')
   t.is(spy.getCall(1).args[1].name, 'recorded_on')
-  t.is(spy.getCall(1).args[1].comparator, '<')
-  t.is(spy.getCall(1).args[1].value, new Date('2018-02-01').getTime())
+  t.is(spy.getCall(1).args[1].comparator, '<=')
+  t.deepEqual(moment(spy.getCall(1).args[1].value).toDate(), moment('2018-01-31').toDate())
 
 })
