59bed4fa0b9efc90b45fd73e3e2009b5ff43be7a
Nicolai Davies <Nicolai.d@hotmail.com>
2018-05-11 13:16:23 +0200

    Fix failing tests

--- test/specs/components/irs_monitor/seasons.test.js
+++ test/specs/components/irs_monitor/seasons.test.js
@@ -4,15 +4,29 @@ import sinon from 'sinon'
 import moment from 'moment'
 import Seasons from 'apps/irs_monitor/pages/controls/seasons.vue'
 
+
 test('it renders', async t => {
-  const wrapper = shallow(Seasons)
+  const mock_store = {
+    $commit() { },
+    state: { irs_monitor: { dashboard_options: { season_start_date: '' } } }
+  }
+  const wrapper = shallow(Seasons, {
+    mocks: { $store: mock_store }
+  })
   t.true(wrapper.exists())
 })
 
 test('is renders an option for each start date', async t => {
+  const mock_store = {
+    $commit() { },
+    state: { irs_monitor: { dashboard_options: { season_start_date: '' } } }
+  }
   const wrapper = shallow(Seasons, {
     propsData: {
       season_start_dates: ['2018-01-01', '2018-02-01']
+    },
+    mocks: {
+      $store: mock_store
     }
   })
 
@@ -23,6 +37,10 @@ test('is renders an option for each start date', async t => {
 })
 
 test('clicking an option calls add_temporal_filter', async t => {
+  const mock_store = {
+    $commit() { },
+    state: { irs_monitor: { dashboard_options: { season_start_date: '' } } }
+  }
   const spy = sinon.spy()
   const wrapper = shallow(Seasons, {
     propsData: {
@@ -30,6 +48,9 @@ test('clicking an option calls add_temporal_filter', async t => {
     },
     methods: {
       set_season: spy
+    },
+    mocks: {
+      $store: mock_store
     }
   })
 
@@ -47,7 +68,8 @@ test('calling set_season with the last date sets start_date to start_date and en
     commit: spy,
     state: {
       irs_monitor:{
-        filters:[]
+        filters:[],
+        dashboard_options: { season_start_date: '' }
       }
     }
   }
@@ -63,19 +85,18 @@ test('calling set_season with the last date sets start_date to start_date and en
   const date_to_test_against = +new Date()
   wrapper.vm.set_season('2018-02-01', 1)
 
-  t.true(spy.calledTwice)
-  t.is(spy.getCall(0).args[0], 'irs_monitor/add_filter')
-  t.is(spy.getCall(0).args[1].name, 'recorded_on')
-  t.is(spy.getCall(0).args[1].comparator, '>=')
-  t.is(spy.getCall(0).args[1].value, new Date('2018-02-01').getTime())
-
-
   t.is(spy.getCall(1).args[0], 'irs_monitor/add_filter')
   t.is(spy.getCall(1).args[1].name, 'recorded_on')
-  t.is(spy.getCall(1).args[1].comparator, '<=')
+  t.is(spy.getCall(1).args[1].comparator, '>=')
+  t.is(spy.getCall(1).args[1].value, new Date('2018-02-01').getTime())
+
+
+  t.is(spy.getCall(2).args[0], 'irs_monitor/add_filter')
+  t.is(spy.getCall(2).args[1].name, 'recorded_on')
+  t.is(spy.getCall(2).args[1].comparator, '<=')
 
   // This is really hard to test, if we just used equal it would fail on a slow machine
-  t.true(spy.getCall(1).args[1].value <= date_to_test_against)
+  t.true(spy.getCall(2).args[1].value <= date_to_test_against)
 })
 
 
@@ -85,7 +106,8 @@ test('calling set_season with the first date sets start_date to start_date and e
     commit: spy,
     state: {
       irs_monitor:{
-        filters:[]
+        filters:[],
+        dashboard_options: { season_start_date: '' }
       }
     }
   }
@@ -101,17 +123,16 @@ test('calling set_season with the first date sets start_date to start_date and e
   wrapper.vm.set_season('2018-01-01', 1)
 
 
-  t.true(spy.calledTwice)
-  t.is(spy.getCall(0).args[0], 'irs_monitor/add_filter')
-  t.is(spy.getCall(0).args[1].name, 'recorded_on')
-  t.is(spy.getCall(0).args[1].comparator, '>=')
-  t.is(spy.getCall(0).args[1].value, new Date('2018-01-01').getTime())
-
-
-
   t.is(spy.getCall(1).args[0], 'irs_monitor/add_filter')
   t.is(spy.getCall(1).args[1].name, 'recorded_on')
-  t.is(spy.getCall(1).args[1].comparator, '<=')
-  t.deepEqual(moment(spy.getCall(1).args[1].value).toDate(), moment('2018-01-31').toDate())
+  t.is(spy.getCall(1).args[1].comparator, '>=')
+  t.is(spy.getCall(1).args[1].value, new Date('2018-01-01').getTime())
+
+
+
+  t.is(spy.getCall(2).args[0], 'irs_monitor/add_filter')
+  t.is(spy.getCall(2).args[1].name, 'recorded_on')
+  t.is(spy.getCall(2).args[1].comparator, '<=')
+  t.deepEqual(moment(spy.getCall(2).args[1].value).toDate(), moment('2018-01-31').toDate())
 
 })
