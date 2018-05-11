15392e74c51e5d52c737dddb86b8609a93a00cd4
Nicolai Davies <Nicolai.d@hotmail.com>
2018-05-11 13:27:31 +0200

    Use debug mode instead of creating our own

--- src/config/analytics.js
+++ src/config/analytics.js
@@ -5,28 +5,18 @@ import VueAnalytics from 'vue-analytics'
 import BUILD_TIME from 'config/build-time'
 
 const instantiate_analytics = (router, store) => {
-  if (BUILD_TIME.DOUMA_PRODUCTION_MODE) {
-    const dimension1 = BUILD_TIME.VERSION_COMMIT_HASH_SHORT // Version
-    const dimension2 = store.state.instance_config.instance.slug // slug
-    const dimension3 = store.state.meta.user ? `${store.state.meta.user.username}/${store.state.meta.user.name}` : 'no user' // User
+  const dimension1 = BUILD_TIME.VERSION_COMMIT_HASH_SHORT // Version
+  const dimension2 = store.state.instance_config.instance.slug // slug
+  const dimension3 = store.state.meta.user ? `${store.state.meta.user.username}/${store.state.meta.user.name}` : 'no user' // User
 
-    Vue.use(VueAnalytics, {
-      id: BUILD_TIME.GA_ANALYTICS_UA,
-      router,
-      set: [{field: 'dimension1', value: dimension1}, {field: 'dimension2', value: dimension2}, {field: 'dimension3', value: dimension3}]
-    })
-  } else {
-    const fake_plugin = {
-      install(Vue, options) {
-        Vue.prototype.$ga = {
-          event() {},
-          set() {},
-          fake_plugin: true
-        }
-      }
+  Vue.use(VueAnalytics, {
+    id: BUILD_TIME.GA_ANALYTICS_UA,
+    router,
+    set: [{field: 'dimension1', value: dimension1}, {field: 'dimension2', value: dimension2}, {field: 'dimension3', value: dimension3}],
+    debug: {
+      sendHitTask: BUILD_TIME.DOUMA_PRODUCTION_MODE
     }
-    Vue.use(fake_plugin)
-  }
+  })
 }
 
 export {instantiate_analytics}
