1a5e082d98684c7d6f6dbc660eea55a618f697f1
Jonathan Smith <jonathan@peoplesized.com>
2018-05-11 12:40:25 +0200

    instantiate analytics _before_ first page render

--- src/config/analytics.js
+++ src/config/analytics.js
@@ -4,11 +4,11 @@ import VueAnalytics from 'vue-analytics'
 
 import BUILD_TIME from 'config/build-time'
 
-const instantiate_analytics = (router, app) => {
+const instantiate_analytics = (router, store) => {
   if (BUILD_TIME.DOUMA_PRODUCTION_MODE) {
     const dimension1 = BUILD_TIME.VERSION_COMMIT_HASH_SHORT // Version
-    const dimension2 = app.$store.state.instance_config.instance.slug // slug
-    const dimension3 = app.$store.state.meta.user ? `${app.$store.state.meta.user.username}/${app.$store.state.meta.user.name}` : 'no user' // User
+    const dimension2 = store.state.instance_config.instance.slug // slug
+    const dimension3 = store.state.meta.user ? `${store.state.meta.user.username}/${store.state.meta.user.name}` : 'no user' // User
 
     Vue.use(VueAnalytics, {
       id: BUILD_TIME.GA_ANALYTICS_UA,
