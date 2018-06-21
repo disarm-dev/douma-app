e0b148ed1b2ee6cd33ebc986a249c3b2fd2d6cef
Jonathan Smith <jonathan@peoplesized.com>
2018-06-21 14:46:30 +0200

    templated

--- src/apps/irs_record_point/pages/suggest_location.vue
+++ src/apps/irs_record_point/pages/suggest_location.vue
@@ -13,17 +13,32 @@
 
       :taggable="true"
       @tag="add_tag"
-      tag-placeholder="Custom location"
       tag-position="bottom"
       :multiple="true"
       :limit="1"
   >
-    <!--<template-->
-        <!--slot="tag"-->
-        <!--slot-scope="{option, search, remove}"-->
-    <!--&gt;-->
-      <!--{{option.name}} {{option.category ? `${option.category}` : 'warning'}}-->
-    <!--</template>-->
+    <template slot="singleLabel" slot-scope="props">{{props.option.name}}</template>
+
+      <template slot="option" slot-scope="props">
+        <span v-if="props.option.name">
+          {{props.option.name}} {{props.option.id}}
+        </span>
+        <span v-else>
+          Use <em>{{props.search}}</em> as a custom entry? This record will not appear on the dashboard.
+        </span>
+    </template>
+
+    <template
+        slot="tag"
+        slot-scope="{option, search, remove}"
+    >
+      <span class="tag" v-if="option.category">
+        {{option.name}} ({{option.category}})
+      </span>
+      <span class="tag warn" v-else>
+        <md-icon>warning</md-icon>Using <em>{{option.name}}</em> as a custom entry - this record will not appear on the dashboard
+      </span>
+    </template>
   </multiselect>
 
 </template>
@@ -59,6 +74,7 @@
         return name + (category ? ` (${category})` : '')
       },
       suggest(query) {
+        if (query.length < 2) return this.suggestion_options
         this.suggestion_options = this.all_locations.filter(l => {
           return l.name
             .toLowerCase()
@@ -67,8 +83,8 @@
       },
       add_tag(tag) {
         const custom_location = {name: tag}
-        // this.suggestion_options.push(custom_location)
         this.location_selection = custom_location
+        this.suggestion_options = [custom_location]
         this.set_location_selection(this.location_selection)
       },
       set_location_selection(location_selection) {
@@ -79,5 +95,12 @@
 </script>
 
 <style scoped>
-
+  .tag {
+    padding: 4px 10px 4px 10px;
+    border-radius: 4px;
+    background-color: #e1e5ff;
+  }
+  .warn {
+    background: #ffc55b;
+  }
 </style>
