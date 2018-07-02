7c3aa2da135d9ffda45a3a4be958064e1ebdbc63
Jonathan Smith <jonathan@peoplesized.com>
2018-07-02 20:25:10 +0200

    stub test file

---
+++ test/specs/components/irs_record_point/list.test.js
@@ -0,0 +1,9 @@
+import {shallow} from 'vue-test-utils'
+import test from 'ava'
+
+import applet from 'apps/irs_record_point/pages/list'
+
+test('0 responses visible when no responses', t => {
+  
+  t.is(true, true)
+})
