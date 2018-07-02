986c65ce28400b12e22e23281015f6c28ec02a6b
Jonathan Smith <jonathan@peoplesized.com>
2018-07-02 14:49:05 +0200

    flatten

--- src/apps/irs_record_point/pages/view_response.vue
+++ src/apps/irs_record_point/pages/view_response.vue
@@ -8,6 +8,15 @@
       <md-card-content>
         <div v-for="section in sections">
           <h4>{{section}}</h4>
+          <div v-if="isObject(flat_section(section))">
+            <div v-for="(val, key) in flat_section(section)">
+              {{key}}: {{val}}
+            </div>
+          </div>
+          <div v-else>
+            {{flat_section(section)}}
+          </div>
+
         </div>
       </md-card-content>
     </md-card>
@@ -15,9 +24,13 @@
 </template>
 
 <script>
+  import flatten from 'flat'
+  import {isDate, isObject} from 'lodash'
+
   import {ResponseController} from 'lib/models/response/controller'
 
   const controller = new ResponseController('record')
+
   export default {
     name: 'view_response',
     props: {
@@ -41,6 +54,7 @@
       this.set_response_by_id(this.response_id)
     },
     methods: {
+      isObject: isObject,
       async set_response_by_id(response_id) {
         const personalised_instance_id = this.$store.state.meta.personalised_instance_id
         const instance = this.$store.state.instance_config.instance.slug
@@ -54,10 +68,25 @@
           console.error('No response found for id', this.response_id)
         }
       },
+      flat_section(section) {
+        if (!this.response || !section) return false
+
+        const chunk = this.response[section]
+
+        if (isObject(chunk) && !isDate(chunk)) {
+          return flatten(chunk)
+        } else if (isDate(chunk)) {
+          return chunk.toLocaleString()
+        } else {
+          return chunk
+        }
+      }
     }
   }
 </script>
 
 <style scoped>
-
+  h4 {
+    margin-bottom: 0;
+  }
 </style>
