09b7ff6e3adfa58bb4d13d95084aee8d68637464
Sihle Mbhamali <mbhamalimosaic@gmail.com>
2018-07-03 13:00:29 +0200

    Passing test

--- test/specs/components/irs_record_point/view_response.test.js
+++ test/specs/components/irs_record_point/view_response.test.js
@@ -5,45 +5,37 @@ import Response from 'apps/irs_record_point/pages/view_response.vue'
 import {ResponseController} from "../../../../src/lib/models/response/controller";
 
 
-test('Should render response', t =>{
+test('Should render response', async t => {
   const mock_store = {
     state: {
       instance_config: {
-        applets: {
-          irs_monitor: {
-            season_start_dates: []
-          }
-        },
-        instance:{
-          slug:'slug'
+        instance: {
+          slug: 'slug'
         }
       },
-      meta:{
-        personalised_instance_id:'personalised_instance_id'
+      meta: {
+        personalised_instance_id: 'personalised_instance_id'
       }
     }
   }
 
-  let read_all_cache = sinon.stub(ResponseController.prototype,'read_all_cache')
-  read_all_cache.resolve([{
-  country:"bwa",
-  id:"7af85a99-87b8-47eb-853f-0a9a120473a7",
-  initial_form_completion_duration_seconds:711,
-  instance_slug:"bwa",
-  personalised_instance_id:"default",
-  recorded_on:"Tue Jul 03 2018 09:43:19 GMT+0200 (South Africa Standard Time)",
-  synced:true,
-  team_name:"Team2",
-  userAgent:"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
-  username:"sm"}])
+  let read_all_cache = sinon.stub(ResponseController.prototype, 'read_all_cache')
+  read_all_cache.resolves([{
+    country: "bwa",
+    id: "7af85a99-87b8-47eb-853f-0a9a120473a7",
+    initial_form_completion_duration_seconds: 711
+  }])
 
-  const wrapper = shallow(Response, {
-  mocks: {
-    $store: mock_store,
-    params:{response_id: 'response.id'}
-  }
-})
+
+  const wrapper = await shallow(Response, {
+    propsData: {response_id: '7af85a99-87b8-47eb-853f-0a9a120473a7'},
+    mocks: {
+      $store: mock_store
+    }
+  })
 
   t.true(wrapper.exists());
+  t.deepEqual(wrapper.vm.sections.length, 3)
+  t.deepEqual(wrapper.find('md-card-header div').text(),'Submitted response review')
 
 })
