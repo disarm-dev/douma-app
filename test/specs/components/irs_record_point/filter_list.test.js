17880b2414a1d9a530846482e7545b6b3d9f84c5
Jonathan Smith <jonathan@peoplesized.com>
2018-07-03 08:56:18 +0200

    refactor

--- test/specs/components/irs_record_point/filter_list.test.js
+++ test/specs/components/irs_record_point/filter_list.test.js
@@ -3,17 +3,14 @@ import {spy, stub} from 'sinon'
 import {shallow} from 'vue-test-utils'
 
 import applet from 'apps/irs_record_point/pages/list'
-import * as Controller from 'lib/models/response/controller'
+import {ResponseController} from 'lib/models/response/controller'
 
+test.beforeEach(t => {
+  ResponseController.prototype.constructor = stub()
+  ResponseController.prototype.read_all_cache = stub().resolves([])
+  ResponseController.prototype.create_records = stub().resolves()
 
-test.beforeEach(() => {
-  Controller.ResponseController.prototype.constructor = stub()
-  Controller.ResponseController.prototype.read_all_cache = stub().resolves([])
-  Controller.ResponseController.prototype.create_records = stub().resolves()
-})
-
-function basic_objects() {
-  const mock_store = {
+  t.context.mock_store = {
     state: {
       instance_config: {
         instance: {}
@@ -30,9 +27,10 @@ function basic_objects() {
     dispatch: () => {
     }
   }
-  const wrapper = shallow(applet, {
+
+  t.context.wrapper = shallow(applet, {
     mocks: {
-      $store: mock_store,
+      $store: t.context.mock_store,
       $router: {
         goBack: () => {
         }
@@ -49,41 +47,33 @@ function basic_objects() {
       },
     },
   })
-
-  return {
-    mock_store,
-    wrapper
-  }
-}
+})
 
 test('controller', async (t) => {
-  const c = new Controller.ResponseController()
+  const c = new ResponseController()
   const actual = await c.read_all_cache()
   t.is(actual.length, 0)
 })
 
 test('0 responses visible when no responses given in test setup', t => {
-  const {mock_store, wrapper} = basic_objects()
-  wrapper.setData({responses: []});
+  t.context.wrapper.setData({responses: []});
 
-  t.is(wrapper.vm.responses.length, 0)
-  t.is(wrapper.find('.md-title').text(), '0 responses (0 unsynced)')
+  t.is(t.context.wrapper.vm.responses.length, 0)
+  t.is(t.context.wrapper.find('.md-title').text(), '0 responses (0 unsynced)')
 })
 
 test('0 responses visible when 0 responses explicitly set', t => {
-  const {mock_store, wrapper} = basic_objects()
-  wrapper.setData({responses: []});
+  t.context.wrapper.setData({responses: []});
 
-  t.is(wrapper.vm.responses.length, 0)
-  t.is(wrapper.find('.md-title').text(), '0 responses (0 unsynced)')
+  t.is(t.context.wrapper.vm.responses.length, 0)
+  t.is(t.context.wrapper.find('.md-title').text(), '0 responses (0 unsynced)')
 })
 
 test('1 response visible', t => {
-  const {mock_store, wrapper} = basic_objects()
-  wrapper.setData({responses: [{id: 1, synced: false, form_data: {a: 1}}]});
+  t.context.wrapper.setData({responses: [{id: 1, synced: false, form_data: {a: 1}}]});
 
-  t.is(wrapper.vm.filtered_responses.length, 1)
-  t.is(wrapper.vm.responses.length, 1)
-  t.is(wrapper.vm.unsynced_count, 1)
-  t.is(wrapper.find('.md-title').text(), '1 responses (1 unsynced)')
+  t.is(t.context.wrapper.vm.filtered_responses.length, 1)
+  t.is(t.context.wrapper.vm.responses.length, 1)
+  t.is(t.context.wrapper.vm.unsynced_count, 1)
+  t.is(t.context.wrapper.find('.md-title').text(), '1 responses (1 unsynced)')
 })
