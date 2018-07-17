7758a065b89978873dfcccdcd9fec5ce4fe30e27
Jonathan Smith <jonathan@peoplesized.com>
2018-07-17 10:40:31 +0200

    destroy shell_app

--- src/shell_app/index.js
+++ src/shell_app/index.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 import Shell from './shell'
 import Login from './login'
+import InstanceConfigs from './instance_configs'
+import {hide_loading_page} from 'config/hide_loading_page'
 
 let shell_app
 
@@ -11,7 +13,17 @@ export function launch_shell_app() {
   const routes = [
     {
       path: '/',
+      name: 'login',
       component: Login,
+    },
+    {
+      path: '/instance_configs',
+      name: 'instance_configs',
+      component: InstanceConfigs,
+    },
+    {
+      path: '*',
+      redirect: '/instance_configs'
     }
   ]
 
@@ -31,12 +43,24 @@ export function launch_shell_app() {
     router,
     store,
     render: createElement => createElement(Shell),
+    beforeDestroy() {
+      console.log('beforeDestroy shell_app')
+    },
+    destroyed() {
+      console.log('shell_app destroyed, remove from DOM')
+    }
   })
+
+  if (shell_app) hide_loading_page()
 }
 
 export function remove_shell_app() {
   if (shell_app) {
-    console.log('shell_app exists - need to remove',)
+    shell_app.$destroy()
+    shell_app.$off()
+    shell_app.$el.remove()
+    shell_app = null
+    console.log('shell_app destroyed')
   } else {
     console.log('shell_app DOES NOT exists - NO need to remove',)
   }
