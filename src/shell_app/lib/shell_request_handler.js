8fd4b73fc82a5d0c12957bed3f39b9d9abe27994
Nicolai Davies <Nicolai.d@hotmail.com>
2018-08-13 14:45:08 +0200

    Get url from common config

--- src/shell_app/lib/shell_request_handler.js
+++ src/shell_app/lib/shell_request_handler.js
@@ -1,9 +1,8 @@
 import axios from 'axios/index'
-import BUILD_TIME from 'config/build-time'
+import CONFIG from 'config/common'
 
 export const shell_axios = axios.create({
-  // baseURL: 'http://localhost:3000/v1',
-  baseURL: BUILD_TIME.BRANCH === 'master' ? 'https://douma.api.disarm.io/v7' : 'https://douma-stage.api.disarm.io/v7',
+  baseURL: CONFIG.api.url + '/' + CONFIG.api.version,
   timeout: 10000,
   headers: { 'API-Key': '' },
   params: { 
