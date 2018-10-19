6ab4b89d333e9ad9ec5a3931652db9c906855e20
mosaic141688 <mbhamalimosaic@gmail.com>
2018-10-19 09:58:14 +0200

    Using the right shape for instance config

--- src/shell_app/pages/Instances.vue
+++ src/shell_app/pages/Instances.vue
@@ -33,7 +33,7 @@
         <li v-for='instance in instances' :key='instance.id' >
           {{instance.name}}
           <ul>
-            <li v-for="config in instance.configs" :key="config.id" @click="get_instance_and_attempt_launch(config.id)">
+            <li v-for="config in instance.configs" :key="config.id" @click="get_instance_and_attempt_launch(config._id)">
               {{instance.name}}@{{config.version}}
             </li>
           </ul>
@@ -116,21 +116,22 @@
         await this.check_geodata_and_launch(instance_config)       
       },
       async check_geodata_and_launch(instance_config) {
+
         // remove permissions for other instances
         const copy_of_user = {...this.$store.state.user} // copy so we don't mutate state, which is bad
         const user = AuthController.prepare_user_for_instance(instance_config.instance, copy_of_user)
         const required = geodata_required(user.permissions)
         
         if (!required) {
-          return launch(instance_config, user)
+          return launch({instance:this.instance_id,instance_config, user})
         }
 
-        configure_spatial_helpers(instance_config.lob)
+        configure_spatial_helpers(instance_config)
 
-        await hydrate_geodata_cache_from_idb(instance_config.lob.instance.slug)
+        await hydrate_geodata_cache_from_idb(instance_config.instance.slug)
         const valid = geodata_in_cache_and_valid()
         if (valid) {
-          launch(instance_config, user)
+          launch({instance:this.instance_id,instance_config, user})
         } else {
           this.$router.push('/geodata')
         }
