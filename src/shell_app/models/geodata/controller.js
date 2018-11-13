bf2567a6edc8293d83b922ec4bb1db66b0607909
mosaic141688 <mbhamalimosaic@gmail.com>
2018-11-13 16:36:56 +0200

    Geodata data is now the whole body

--- src/shell_app/models/geodata/controller.js
+++ src/shell_app/models/geodata/controller.js
@@ -5,7 +5,7 @@ import CONFIG from 'config/common'
 export async function get_and_save_layer({ level_name, instance_slug, instance_id, geodata_version}) {
   const res = await Remote.get_geodata_layer({ level_name, instance_id})
 
-  const geodata_layer = res.data.file.geodata_data
+  const geodata_layer = res.data
 
   await save_geodata_to_idb({ level_name, level_geodata: geodata_layer, instance_slug, geodata_version })
 }
