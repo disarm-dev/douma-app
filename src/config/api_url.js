4162d14ed6f464a4ad7b9a02555ef2469fcf16af
Jonathan Smith <jonathan@peoplesized.com>
2018-11-14 17:00:23 +0200

    production vs dev hostname for API

--- src/config/api_url.js
+++ src/config/api_url.js
@@ -3,8 +3,14 @@ import CONFIG from 'config/common'
 export function get_api_url() {
   const protocol = CONFIG.api.protocol
   const host = location.hostname
+  const production_host = `api.${host}`
   const port = CONFIG.api.port
   const version = CONFIG.api.version
 
-  return `${protocol}://${host}:${port}/${version}`
+  if (__DOUMA_PRODUCTION_MODE) {
+    return `${protocol}://${production_host}/${version}`
+  } else {
+    return `${protocol}://${host}:${port}/${version}`
+  }
+
 }
